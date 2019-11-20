561b956453662eb5e16983690414a4d6cdc95b3c
Mike Atkisson <mpatkisson@gmail.com>
2019-11-19 23:26:34 -0500

    Adds submenues to the sidebar

--- src/src/app/sidebar/sidebar.component.ts
+++ src/src/app/sidebar/sidebar.component.ts
@@ -7,7 +7,23 @@ declare interface RouteInfo {
     icon: string;
     class: string;
 }
+declare interface MenuItem {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+declare interface MenuHeader {
+    path: string;
+    marker: string;
+    title: string;
+    icon: string;
+    class: string;
+    items: MenuItem[];
+}
+
 export const ROUTES: RouteInfo[] = [
+    { path: '/echad', title: 'Echad',  icon:'', class: '' },
     { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
     { path: '/user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
     { path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
@@ -18,17 +34,56 @@ export const ROUTES: RouteInfo[] = [
     { path: '/upgrade', title: 'Upgrade to PRO',  icon:'pe-7s-rocket', class: 'active-pro' },
 ];
 
+export const MENU_HEADERS: MenuHeader[] = [
+    { 
+      path: '',
+      marker: '\u05D0',
+      title: 'Echad',
+      icon:'',
+      class: '',
+      items: [
+        { path: '/dashboard', title: 'Dashboard',  icon: 'pe-7s-graph', class: '' },
+        { path: '/user', title: 'User Profile',  icon:'pe-7s-user', class: '' },
+        { path: '/table', title: 'Table List',  icon:'pe-7s-note2', class: '' },
+        { path: '/typography', title: 'Typography',  icon:'pe-7s-news-paper', class: '' },
+        { path: '/icons', title: 'Icons',  icon:'pe-7s-science', class: '' },
+        { path: '/notifications', title: 'Notifications',  icon:'pe-7s-bell', class: '' },
+      ]
+    }
+];
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent implements OnInit {
   menuItems: any[];
+  menuHeaders: any[];
 
   constructor() { }
 
   ngOnInit() {
     this.menuItems = ROUTES.filter(menuItem => menuItem);
+    this.menuHeaders = MENU_HEADERS.filter(menuHeader => menuHeader);
+
+    // These three are need to make submenus work.
+    $(document).on('hide.bs.dropdown', '.keepopen', function () {
+      return false;
+    });
+
+    $('body').on("click", "li.active", function () {
+      if ($(this).hasClass('keepopen')) {
+        $(this).removeClass('keepopen');
+      } else {
+        $(this).addClass('keepopen');
+      }
+    });
+
+    $('body').on("click", "li.active.open ul.dropdown-menu", function (e) {
+      $(this).parent().is(".open") && e.stopPropagation();
+    });
+    // These three are need to make submenus work.
+
   }
   isMobileMenu() {
       if ($(window).width() > 991) {
